b24604541973cbc56bce660f5edf032db0d123ff
AlbaErik <erik.alba97@gmail.com>
2021-05-28 23:39:15 -0300

    Benchmark

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,20 +2,27 @@
 
 import kotlin.math.pow
 import kotlin.math.sqrt
+import kotlin.random.Random
+import kotlin.system.measureTimeMillis
 
+const val max = 1000000000.00
 
 fun main() {
-    val asd = System.currentTimeMillis()
-    val p1 = Punto(2.0,3.0)
-    val p2 = Punto(12.0,30.0)
-    val p3 = Punto(40.0,50.0)
-    val p4 = Punto(5.0,1.0)
-    val p5 = Punto(12.0,10.0)
-    val p6 = Punto(3.0,4.0)
 
-    val lista: List<Punto> = listOf(p1,p2,p3,p4,p5,p6)
+    val lista = benchmarck(19)
+    val tiempoInicio = System.currentTimeMillis()
 
-    println("La distancia minima es:  ${distanciaMinimaCShell(lista)} y el tiempo es: ${System.currentTimeMillis()-asd}")
+    println("La distancia minima es:  ${distanciaMinimaA(lista)} y el tiempo es: ${System.currentTimeMillis()-tiempoInicio}")
+}
+
+private fun benchmarck(potencia: Int):List<Punto>{
+    val n = 2.00
+    val cantPuntos = n.pow(potencia).toInt()
+    var list: MutableList<Punto> = mutableListOf()
+    for (i in 1..cantPuntos)
+        list.add( Punto( Random.nextDouble(max), Random.nextDouble(max) ) )
+
+    return list
 }
 
 private fun distancia(p1: Punto, p2: Punto): Double =
@@ -26,9 +33,10 @@ private fun distanciaMinimaA(list: List<Punto>): ParDePuntos{
     val pMin = Punto(0.0,0.0)
     val pMax = Punto(max, max)
     var ret = ParDePuntos(pMin, pMax, distancia(pMin, pMax))
+    var dis: Double
     for (p in list){
         for (k in list){
-            val dis = distancia(p,k)
+            dis = distancia(p,k)
             if(p != k && dis < ret.d) {
                 ret = ParDePuntos(p, k, dis)
             }
@@ -45,7 +53,9 @@ private fun distanciaMinimaB(list: List<Punto>): ParDePuntos{
         val par1 = distanciaMinimaB(list.filter { it.x <= m })
         val par2 = distanciaMinimaB(list.filter { it.x > m })
         val parMenor1 = if(par1.d < par2.d) par1 else par2
-        val franjaOrdenada = list.filter { it.x in (m - parMenor1.d).. (m + parMenor1.d) }.sortedBy { it.y }
+        val franjaOrdenada = list.filter {
+            it.x in (m - parMenor1.d) .. (m + parMenor1.d)
+        }.sortedBy { it.y }
         val parMenor2 = recorrerFranja(franjaOrdenada, m)
         if (parMenor2 != null && parMenor1.d < parMenor2.d) parMenor2 else parMenor1
     }
